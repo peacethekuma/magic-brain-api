f546cf83de6f933a113c73ce7a8805944e442c93
peacethekuma <peacethekuma@gmail.com>
2018-12-02 18:35:31 +0800

    Add port

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-<<<<<<< HEAD
-
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-
-
-=======
 const knex = require('knex');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
@@ -28,7 +20,6 @@ const db = knex({
 
 
 const app = express();
->>>>>>> development
 
 // middleware
 app.use(cors());
@@ -36,48 +27,10 @@ app.use(bodyParser.json());
 
 
 //sign in
-<<<<<<< HEAD
-app.post('/signin', (req, res) => {
-  console.log(req.body);
-  
-  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    return res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in')
-  }
-})
-
-//register
-app.post('/register', (req, res) => {
-
-  const {
-    email,
-    password,
-    name
-  } = req.body;
-
-  bcrypt.hash(password, null, null, function (err, hash) {
-    // Store hash in your password DB.
-    console.log(hash);
-  });
-
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    // password: password,
-    entries: 0,
-    joined: new Date()
-  });
-
-  res.json(database.users[database.users.length - 1]);
-})
-=======
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
 //register
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
->>>>>>> development
 
 //profile
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
@@ -86,6 +39,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
