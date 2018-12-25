2b4dad980a82020d2b4c5c99ded155dbfe17acc4
peacethekuma <peacethekuma@gmail.com>
2018-12-25 08:25:53 +0800

    Add image upload end-point

--- controllers/image.js
+++ controllers/image.js
@@ -1,8 +1,15 @@
 const Clarifai = require('clarifai');
+const cloudinary = require('cloudinary');
 
 const app = new Clarifai.App({
   apiKey: process.env.CLARIFAI_APIKEY
 });
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUDINAEY_APIKEY, 
+  api_secret: process.env.CLOUDINAEY_APISECRET
+})
 
 const handleApiCall = (req, res) => {
   app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
@@ -11,7 +18,6 @@ const handleApiCall = (req, res) => {
     })
     .catch(err => res.status(400).json('unable to work with API'))
 }
-
 
 
 const handleImage = (req, res, db) => {
@@ -26,7 +32,20 @@ const handleImage = (req, res, db) => {
     .catch(err => res.status(400).json('unable to get entries'))
 }
 
+const uploadImage = (req, res) => {
+  const values = Object.values(req.files)
+  const promises = values.map(image => cloudinary.uploader.upload(image.path))
+
+  Promise
+    .all(promises)
+    .then(results => res.json(results))
+    .catch((err) => res.status(400).json(err))
+}
+
+
+
 module.exports = {
   handleImage: handleImage,
-  handleApiCall: handleApiCall
+  handleApiCall: handleApiCall,
+  uploadImage:uploadImage
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const formData = require('express-form-data');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
@@ -12,7 +13,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl:true,
+    ssl: true,
   }
 });
 
@@ -22,8 +23,9 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(bodyParser.json());
+app.use(formData.parse());
 
-app.get('/',(req,res)=>{res.send('it is working!')});
+app.get('/', (req, res) => { res.send('it is working!') });
 
 //sign in
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
@@ -37,6 +39,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) }
 //image
 app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+app.post('/image-upload', (req, res) => { image.uploadImage(req, res) });
+
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`app is running on port ${process.env.PORT}`);
